64a08492d42f3af5e15e7443972652627872d72a
Benjamin Ong <benjamin.ong@trentglobal.edu.sg>
2022-06-06 09:27:59 +0000

    completed hands on CR

--- index.js
+++ index.js
@@ -8,10 +8,25 @@ const app = express();
 app.set('view engine', 'hbs');
 
 wax.on(hbs.handlebars);
-wax.setLayoutPath('.views/layouts');
+wax.setLayoutPath('views/layouts');
+
+//enable forms
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', function(req,res){
-    res.send("It's alive!")
+    res.render('all-books', {'books': database.getAll()})
+})
+
+app.get('/create', function(req,res){
+    res.render('create-book')
+})
+
+app.post('/create', function(req,res){
+    console.log(req.body)
+    let title = req.body.title
+    let isbn = req.body.isbn
+    let insertedId = database.addBook(title, isbn);
+    res.redirect('/')
 })
 
 app.listen(3000, function(){
