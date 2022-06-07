080a61f691ed4dc7b3a9a28a89bb9c022049f534
Benjamin Ong <benjamin.ong@trentglobal.edu.sg>
2022-06-07 06:15:44 +0000

    completed CRUD for this hands-on

--- index.js
+++ index.js
@@ -22,10 +22,56 @@ app.get('/create', function(req,res){
 })
 
 app.post('/create', function(req,res){
-    console.log(req.body)
     let title = req.body.title
     let isbn = req.body.isbn
     let insertedId = database.addBook(title, isbn);
+    res.redirect('/')
+})
+
+//rendering update page
+app.get('/update/:book_id', function(req,res){
+    let bookData = database.getBook(req.params.book_id)
+    let title = bookData.title
+    let isbn= bookData.isbn
+    let payload = {
+        'title' : title,
+        'isbn' : isbn
+    }
+    res.render('update-book', payload)
+})
+
+//updating database with data from user input form
+app.post('/update/:book_id', function(req,res){
+    let updateData = req.body
+    let title = updateData.title
+    let isbn = updateData.isbn
+    let bookId = req.params.book_id
+    payload = {
+        'title' : title,
+        'isbn' : isbn,
+    }
+    database.updateBook(bookId, title, isbn)
+    res.redirect('/')
+})
+
+// sample of bookData {"id":123074,"title":"Lord of the Flies","isbn":"82tu47t9ureioute"}
+
+//delete
+app.get('/delete/:book_id', function(req,res){
+    let bookId = req.params.book_id;
+    let bookData = database.getBook(bookId)
+    let title = bookData.title
+    let isbn = bookData.isbn
+    payload = {
+        'title' : title,
+        'isbn' : isbn
+    }
+    res.render('delete-book', payload)
+})
+
+app.post('/delete/:book_id', function(req,res){
+    let bookId = req.params.book_id;
+    database.deleteBook(bookId);
     res.redirect('/')
 })
 
